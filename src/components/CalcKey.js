ae830942c7808ea671264106d9f3a49896cec2ec
Diego Luna <slowkingvii@gmail.com>
2022-08-17 17:08:13 -0500

    Implement calculate logic into Calculator

--- src/components/CalcKey.js
+++ src/components/CalcKey.js
@@ -4,19 +4,26 @@ import PropTypes from 'prop-types';
 class CalcKey extends Component {
   constructor(props) {
     super(props);
+    this.handleKeyClick = this.handleKeyClick.bind(this);
     this.state = {};
+  }
+
+  handleKeyClick() {
+    const { label, handleKeyClick } = this.props;
+    handleKeyClick(label);
   }
 
   render() {
     const { keyClass, label, colored } = this.props;
     const colorClass = colored ? ' colored' : '';
-    return <button className={`key ${keyClass}${colorClass}`} type="button">{label}</button>;
+    return <button className={`key ${keyClass}${colorClass}`} type="button" onClick={this.handleKeyClick}>{label}</button>;
   }
 }
 
 CalcKey.propTypes = {
   keyClass: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
+  handleKeyClick: PropTypes.func.isRequired,
   colored: PropTypes.bool,
 };
 CalcKey.defaultProps = { colored: false };
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,37 +2,63 @@ import './css/Calculator.css';
 
 import { Component } from 'react';
 import CalcKey from './CalcKey';
+import calculate from './logic/calculate';
 
 class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.state = { result: 0 };
+    this.handleKeyClick = this.handleKeyClick.bind(this);
+    this.data = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.state = {
+      display: '0',
+      prev: null,
+      operation: null,
+    };
+  }
+
+  handleKeyClick(keyLabel) {
+    this.data = calculate(this.data, keyLabel);
+
+    const { total, next, operation } = this.data;
+    this.setState({
+      display: next || total || '0',
+      prev: next ? total : null,
+      operation,
+    });
   }
 
   render() {
-    const { result } = this.state;
+    const { display, prev, operation } = this.state;
     return (
       <div className="calculator">
-        <div className="display"><span>{result}</span></div>
-        <CalcKey keyClass="allClear" label="AC" />
-        <CalcKey keyClass="memoryRecall" label="+/-" />
-        <CalcKey keyClass="percent" label="%" />
-        <CalcKey keyClass="divide" label="&divide;" colored />
-        <CalcKey keyClass="seven" label="7" />
-        <CalcKey keyClass="eigth" label="8" />
-        <CalcKey keyClass="nine" label="9" />
-        <CalcKey keyClass="multiply" label="x" colored />
-        <CalcKey keyClass="four" label="4" />
-        <CalcKey keyClass="five" label="5" />
-        <CalcKey keyClass="six" label="6" />
-        <CalcKey keyClass="minus" label="-" colored />
-        <CalcKey keyClass="one" label="1" />
-        <CalcKey keyClass="two" label="2" />
-        <CalcKey keyClass="three" label="3" />
-        <CalcKey keyClass="plus" label="+" colored />
-        <CalcKey keyClass="zero" label="0" />
-        <CalcKey keyClass="dot" label="." />
-        <CalcKey keyClass="equal" label="=" colored />
+        <div className="display">
+          <span className="prev">{prev}</span>
+          <span>{operation}</span>
+          <span>{display}</span>
+        </div>
+        <CalcKey keyClass="allClear" label="AC" handleKeyClick={this.handleKeyClick} />
+        <CalcKey keyClass="plus-minus" label="+/-" handleKeyClick={this.handleKeyClick} />
+        <CalcKey keyClass="percent" label="%" handleKeyClick={this.handleKeyClick} />
+        <CalcKey keyClass="divide" label="÷" handleKeyClick={this.handleKeyClick} colored />
+        <CalcKey keyClass="seven" label="7" handleKeyClick={this.handleKeyClick} />
+        <CalcKey keyClass="eigth" label="8" handleKeyClick={this.handleKeyClick} />
+        <CalcKey keyClass="nine" label="9" handleKeyClick={this.handleKeyClick} />
+        <CalcKey keyClass="multiply" label="x" handleKeyClick={this.handleKeyClick} colored />
+        <CalcKey keyClass="four" label="4" handleKeyClick={this.handleKeyClick} />
+        <CalcKey keyClass="five" label="5" handleKeyClick={this.handleKeyClick} />
+        <CalcKey keyClass="six" label="6" handleKeyClick={this.handleKeyClick} />
+        <CalcKey keyClass="minus" label="-" handleKeyClick={this.handleKeyClick} colored />
+        <CalcKey keyClass="one" label="1" handleKeyClick={this.handleKeyClick} />
+        <CalcKey keyClass="two" label="2" handleKeyClick={this.handleKeyClick} />
+        <CalcKey keyClass="three" label="3" handleKeyClick={this.handleKeyClick} />
+        <CalcKey keyClass="plus" label="+" handleKeyClick={this.handleKeyClick} colored />
+        <CalcKey keyClass="zero" label="0" handleKeyClick={this.handleKeyClick} />
+        <CalcKey keyClass="dot" label="." handleKeyClick={this.handleKeyClick} />
+        <CalcKey keyClass="equal" label="=" handleKeyClick={this.handleKeyClick} colored />
       </div>
     );
   }
