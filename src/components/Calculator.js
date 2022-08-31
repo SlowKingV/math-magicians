ac79e3be854058487d8e46d4feb6c6f8f474fcf5
Diego Luna <slowkingvii@gmail.com>
2022-08-31 10:30:09 -0500

    Refactor CalcKey into key array

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,6 +4,38 @@ import { useState } from 'react';
 import CalcKey from './CalcKey';
 import calculate from './logic/calculate';
 
+const keys = [
+  { className: 'allClear', label: 'AC', id: 1 },
+  { className: 'plus-minus', label: '+/-', id: 2 },
+  { className: 'percent', label: '%', id: 3 },
+  {
+    className: 'divide', label: '÷', colored: true, id: 4,
+  },
+  { className: 'seven', label: '7', id: 5 },
+  { className: 'eigth', label: '8', id: 6 },
+  { className: 'nine', label: '9', id: 7 },
+  {
+    className: 'multiply', label: 'x', colored: true, id: 8,
+  },
+  { className: 'four', label: '4', id: 9 },
+  { className: 'five', label: '5', id: 10 },
+  { className: 'six', label: '6', id: 11 },
+  {
+    className: 'minus', label: '-', colored: true, id: 12,
+  },
+  { className: 'one', label: '1', id: 13 },
+  { className: 'two', label: '2', id: 14 },
+  { className: 'three', label: '3', id: 15 },
+  {
+    className: 'plus', label: '+', colored: true, id: 16,
+  },
+  { className: 'zero', label: '0', id: 17 },
+  { className: 'dot', label: '.', id: 18 },
+  {
+    className: 'equal', label: '=', colored: true, id: 19,
+  },
+];
+
 let data = {
   total: null,
   next: null,
@@ -26,30 +58,20 @@ const Calculator = () => {
 
   return (
     <div className="calculator">
-      <div className="display">
+      <div className="display noselect">
         <span className="prev">{prev}</span>
         <span>{operation}</span>
         <span>{display}</span>
       </div>
-      <CalcKey keyClass="allClear" label="AC" handleKeyClick={handleKeyClick} />
-      <CalcKey keyClass="plus-minus" label="+/-" handleKeyClick={handleKeyClick} />
-      <CalcKey keyClass="percent" label="%" handleKeyClick={handleKeyClick} />
-      <CalcKey keyClass="divide" label="÷" handleKeyClick={handleKeyClick} colored />
-      <CalcKey keyClass="seven" label="7" handleKeyClick={handleKeyClick} />
-      <CalcKey keyClass="eigth" label="8" handleKeyClick={handleKeyClick} />
-      <CalcKey keyClass="nine" label="9" handleKeyClick={handleKeyClick} />
-      <CalcKey keyClass="multiply" label="x" handleKeyClick={handleKeyClick} colored />
-      <CalcKey keyClass="four" label="4" handleKeyClick={handleKeyClick} />
-      <CalcKey keyClass="five" label="5" handleKeyClick={handleKeyClick} />
-      <CalcKey keyClass="six" label="6" handleKeyClick={handleKeyClick} />
-      <CalcKey keyClass="minus" label="-" handleKeyClick={handleKeyClick} colored />
-      <CalcKey keyClass="one" label="1" handleKeyClick={handleKeyClick} />
-      <CalcKey keyClass="two" label="2" handleKeyClick={handleKeyClick} />
-      <CalcKey keyClass="three" label="3" handleKeyClick={handleKeyClick} />
-      <CalcKey keyClass="plus" label="+" handleKeyClick={handleKeyClick} colored />
-      <CalcKey keyClass="zero" label="0" handleKeyClick={handleKeyClick} />
-      <CalcKey keyClass="dot" label="." handleKeyClick={handleKeyClick} />
-      <CalcKey keyClass="equal" label="=" handleKeyClick={handleKeyClick} colored />
+      {keys.map((key) => (
+        <CalcKey
+          key={key.id}
+          keyClass={key.className}
+          label={key.label}
+          handleKeyClick={handleKeyClick}
+          colored={key.colored}
+        />
+      ))}
     </div>
   );
 };
